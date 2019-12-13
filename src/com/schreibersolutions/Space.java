a2fc513720af1afd994bfc718639fd474082a843
Dan Schreiber <dmschreiber@gmail.com>
2019-12-12 20:08:28 -0500

    add lcm function

--- src/com/schreibersolutions/Space.java
+++ src/com/schreibersolutions/Space.java
@@ -134,7 +134,23 @@ public class Space {
         }
 
         System.out.printf("<%d, %d, %d>\n", seeds[0], seeds[1], seeds[2]);
+        System.out.println(lcm(lcm(seeds[0],seeds[1]),seeds[2]));
+        
 
+    }
 
+    public long lcm(long number1, long number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
+        }
+        long absNumber1 = Math.abs(number1);
+        long absNumber2 = Math.abs(number2);
+        long absHigherNumber = Math.max(absNumber1, absNumber2);
+        long absLowerNumber = Math.min(absNumber1, absNumber2);
+        long lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
+        }
+        return lcm;
     }
 }
