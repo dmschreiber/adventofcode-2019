472232567b90fb3c626a2108bd130de32ee817f2
Dan Schreiber <dmschreiber@gmail.com>
2020-11-26 21:03:20 -0500

    dec 16 part 2

--- src/com/schreibersolutions/FFT.java
+++ src/com/schreibersolutions/FFT.java
@@ -1,14 +1,19 @@
 package com.schreibersolutions;
 
+import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class FFT {
     private boolean LOG = false;
 
     private int[] base_pattern;
 
+    public int offset = 0;
     public FFT(int[] pattern) {
         base_pattern = pattern;
     }
@@ -36,52 +41,51 @@ public class FFT {
         return retval;
     }
 
-    private int calculateRow(int index) {
+    private int calculateRow(int index, char[] sequence) {
 
+        int sequence_length = sequence.length;
+        int sub_index = 0;
+        int total = 0;
+        int master_index = 0;
+        int repeat_length = (index+1)*base_pattern.length;
+        // for sub_index: index+master_index*repeat_length to index*2+master_index*repeat_length; -> 1
+        while (index+master_index*repeat_length < sequence_length) {
+            for (sub_index = (index+master_index*repeat_length); sub_index < (index*2+master_index*repeat_length+1); sub_index++) {
+                if (sub_index >= sequence_length) break;
+                char sub_c = sequence[sub_index];
+                total += (sub_c - '0'); // * pattern[sub_index]; HARD CODED
+
+            }
+            for (sub_index = (index+repeat_length/2+master_index*repeat_length); sub_index < (index*2+repeat_length/2+master_index*repeat_length+1); sub_index++) {
+                if (sub_index >= sequence_length) break;
+                char sub_c = sequence[sub_index];
+                total -= (sub_c - '0'); // * pattern[sub_index]; HARD CODED
+
+            }
+            ++master_index;
+        }
+
+//        if (index % 10000 == 0) System.out.printf("Finished char %d of %d\n", index, sequence_length);
+        if (LOG) System.out.printf(" = %d \n", total);
+        total = Math.abs(total);
+        total = total % 10;
+        return total;
 
     }
 
     public char[] processPhase (char[] sequence) {
         char[] retval = new char[sequence.length];
         int index = 0;
-        long calcs = 0;
-        LocalDateTime start = LocalDateTime.now();
 
-        for (index = 0; index < sequence.length; index++) {
-            //int[] pattern = getPattern(index+1, sequence.length);
-
-            int sequence_length = sequence.length;
-            int sub_index = 0;
-            int total = 0;
-            int master_index = 0;
-            int repeat_length = (index+1)*base_pattern.length;
-            // for sub_index: index+master_index*repeat_length to index*2+master_index*repeat_length; -> 1
-            while (index+master_index*repeat_length < sequence_length) {
-                for (sub_index = (index+master_index*repeat_length); sub_index < (index*2+master_index*repeat_length+1); sub_index++) {
-                    if (sub_index >= sequence_length) break;
-                    char sub_c = sequence[sub_index];
-                    total += (sub_c - '0'); // * pattern[sub_index]; HARD CODED
-
-                    ++calcs;
-
-                }
-                for (sub_index = (index+repeat_length/2+master_index*repeat_length); sub_index < (index*2+repeat_length/2+master_index*repeat_length+1); sub_index++) {
-                    if (sub_index >= sequence_length) break;
-                    char sub_c = sequence[sub_index];
-                    total -= (sub_c - '0'); // * pattern[sub_index]; HARD CODED
-
-                    ++calcs;
-
-                }
-                ++master_index;
-            }
-
-            if (index % 10000 == 0) System.out.printf("Finished char %d of %d at %d (%d calcs)\n", index, sequence.length, start.until(LocalDateTime.now(), ChronoUnit.SECONDS),calcs);
-            if (LOG) System.out.printf(" = %d \n", total);
-            total = Math.abs(total);
-            total = total % 10;
-            retval[index] = (char) ('0' + total);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (index = offset; index < sequence.length; index++) {
+            list.add(index);
         }
+
+        list.parallelStream().forEach((Integer row) -> {
+            retval[row] = (char) ('0' + calculateRow(row, sequence));
+        });
+
         return retval;
     }
 
@@ -96,8 +100,10 @@ public class FFT {
     }
 
     public char[] multiplyByTenThousand(char[] input) {
-        char[] retval = new char[input.length*10000];
-        for (int i = 0; i< 10000; i++) {
+        int m = 10000;
+
+        char[] retval = new char[input.length*m];
+        for (int i = 0; i< m; i++) {
             for (int j = 0; j<input.length; j++) {
                 retval[i*input.length + j] = input[j];
             }
@@ -116,12 +122,15 @@ public class FFT {
         output = sb.toString();
         return output;
     }
+
+
     public char[] processPhase(char[] input, int phase_count) {
         char[] output = input;
 
         for (int i = 0; i < phase_count; i++) {
-            System.out.printf("Starting phase %d\n", i);
+            LocalDateTime start = LocalDateTime.now();
             output = processPhase(output);
+            System.out.printf("Completed phase %d in %d seconds\n", i,start.until(LocalDateTime.now(),ChronoUnit.SECONDS));
         }
         return output;
 
@@ -132,6 +141,7 @@ public class FFT {
 
         for (int i = 0; i < phase_count; i++) {
             output = processPhase(output);
+//            System.out.printf("%3d-%s\n", i,String.valueOf(output));
         }
         return output;
     }
--- src/com/schreibersolutions/Main.java
+++ src/com/schreibersolutions/Main.java
@@ -167,6 +167,8 @@ public class Main {
 
     public void dec16() {
         String input = "12345678";
+        char[] input_char;
+        char[] output_char;
         FFT fft = new FFT(new int[]{0,1,0,-1});
         boolean testPassed;
 
@@ -196,38 +198,35 @@ public class Main {
         testPassed = (output.substring(0,8).compareTo("73745418") == 0);
         System.out.printf((testPassed ? ANSI_RESET : ANSI_RED) + "Test : %s\n", testPassed);
         input = "69317163492948606335995924319873";
-        output = fft.processPhase(input, 100);
-        testPassed = (output.substring(0,8).compareTo("52432133") == 0);
+//        input = "00000000000000000000995924319873";
+        input_char = input.toCharArray();
+        output_char = fft.processPhase(input_char, 100);
+        testPassed = (String.valueOf(output_char).substring(0,8).compareTo("52432133") == 0);
         System.out.printf((testPassed ? ANSI_RESET : ANSI_RED) + "Test : %s\n", testPassed);
 
         input = "03036732577212944063491565474664";
-        input = "59773590431003134109950482159532121838468306525505797662142691007448458436452137403459145576019785048254045936039878799638020917071079423147956648674703093863380284510436919245876322537671069460175238260758289779677758607156502756182541996384745654215348868695112673842866530637231316836104267038919188053623233285108493296024499405360652846822183647135517387211423427763892624558122564570237850906637522848547869679849388371816829143878671984148501319022974535527907573180852415741458991594556636064737179148159474282696777168978591036582175134257547127308402793359981996717609700381320355038224906967574434985293948149977643171410237960413164669930";
+//        input = "59773590431003134109950482159532121838468306525505797662142691007448458436452137403459145576019785048254045936039878799638020917071079423147956648674703093863380284510436919245876322537671069460175238260758289779677758607156502756182541996384745654215348868695112673842866530637231316836104267038919188053623233285108493296024499405360652846822183647135517387211423427763892624558122564570237850906637522848547869679849388371816829143878671984148501319022974535527907573180852415741458991594556636064737179148159474282696777168978591036582175134257547127308402793359981996717609700381320355038224906967574434985293948149977643171410237960413164669930";
+        input_char = input.toCharArray();
 
         int offset = Integer.valueOf(input.substring(0,7));
-        char[] input_char = input.toCharArray();
 
-        System.out.printf("Start %s\n", LocalDateTime.now());
         input_char = fft.multiplyByTenThousand(input_char);
-        System.out.printf("End multiply %s\n", LocalDateTime.now());
-        char[] output_char = fft.processPhase(input_char, 100);
-//        testPassed = (output_char[offset] == '8');
-//        testPassed = (output_char[offset+1] == '4') && testPassed;
-//        testPassed = (output_char[offset+2] == '4') && testPassed;
-//        testPassed = (output_char[offset+3] == '6') && testPassed;
-//        testPassed = (output_char[offset+4] == '2') && testPassed;
-//        testPassed = (output_char[offset+5] == '0') && testPassed;
-//        testPassed = (output_char[offset+6] == '2') && testPassed;
-//        testPassed = (output_char[offset+7] == '6') && testPassed;
-//        System.out.printf((testPassed ? ANSI_RESET : ANSI_RED) + "Test : %s\n", testPassed);
+        // Only need to calculate starting at the offset
+        fft.offset = offset;
+
+        output_char = fft.processPhase(input_char, 100);
+        testPassed = (output_char[offset] == '8');
+        testPassed = (output_char[offset+1] == '4') && testPassed;
+        testPassed = (output_char[offset+2] == '4') && testPassed;
+        testPassed = (output_char[offset+3] == '6') && testPassed;
+        testPassed = (output_char[offset+4] == '2') && testPassed;
+        testPassed = (output_char[offset+5] == '0') && testPassed;
+        testPassed = (output_char[offset+6] == '2') && testPassed;
+        testPassed = (output_char[offset+7] == '6') && testPassed;
+        System.out.printf((testPassed ? ANSI_RESET : ANSI_RED) + "Test : %s\n", testPassed);
         for (int i = 0; i < 8; i++) {
             System.out.print(String.valueOf(output_char[offset+i]));
         }
-//        System.out.println(LocalDateTime.now());
-//        input = "59773590431003134109950482159532121838468306525505797662142691007448458436452137403459145576019785048254045936039878799638020917071079423147956648674703093863380284510436919245876322537671069460175238260758289779677758607156502756182541996384745654215348868695112673842866530637231316836104267038919188053623233285108493296024499405360652846822183647135517387211423427763892624558122564570237850906637522848547869679849388371816829143878671984148501319022974535527907573180852415741458991594556636064737179148159474282696777168978591036582175134257547127308402793359981996717609700381320355038224906967574434985293948149977643171410237960413164669930";
-//        output = fft.processPhase(input, 100);
-//        System.out.printf("result: %s\n", output.substring(0,8));
-//        System.out.println(LocalDateTime.now());
-
 
     }
     public static void main(String[] args) {
