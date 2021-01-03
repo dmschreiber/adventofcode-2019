ec4c42e0195a99be30f49114689bbb463200d074
Dan Schreiber <dmschreiber@gmail.com>
2021-01-03 16:04:22 -0500

    finally solving Day 22 part 2

--- aoc/src/dec22.rs
+++ aoc/src/dec22.rs
@@ -9,7 +9,8 @@ mod tests {
       super::solve_part1(&lines, 10007);
       println!("Complete in {:?}", start.elapsed());
 
-      super::solve_part2(&lines,119315717514047);
+      let lines: Vec<String> = include_str!("../inputs/dec22.txt").lines().map(|s| (&*s).to_string() ).collect();
+      super::solve_part2(&lines,119315717514047, 101741582076661);
     }
   }
   
@@ -71,6 +72,40 @@ fn single_transform (op : &Ops, stack_size : i128, input : i128) -> i128 {
   }
 }
 
+fn all_composite (ops: &Vec<Ops>, stack_size : i128) -> (i128, i128) {
+  let mut input = (0,1);
+  for op in ops {
+    input = single_composite(op, stack_size, input.0, input.1);
+  }
+  return input;
+}
+
+fn single_composite (op : &Ops, stack_size : i128, add : i128, mult : i128) -> (i128,i128) {
+  let ret = match op {
+    Ops::NewStack => ((stack_size - add - 1).checked_rem_euclid(stack_size).unwrap(),-1*mult),
+    Ops::CutInto(num) => ((add - *num as i128).checked_rem_euclid(stack_size).unwrap(),mult),
+    Ops::Transpose(num) => (add*(*num as i128).checked_rem_euclid(stack_size).unwrap(),(mult*(*num as i128)).checked_rem_euclid(stack_size).unwrap())
+  };
+  return ret;
+}
+
+fn all_inverse_composite (ops: &Vec<Ops>, stack_size : i128) -> (i128, i128) {
+  let mut input = (0,1);
+  for op in ops.iter().rev() {
+    input = single_inverse_composite(op, stack_size, input.0, input.1);
+  }
+  return input;
+}
+
+fn single_inverse_composite (op : &Ops, stack_size : i128, add : i128, mult : i128) -> (i128,i128) {
+  let ret = match op {
+    Ops::NewStack => ((stack_size - add - 1).checked_rem_euclid(stack_size).unwrap(),(-1*mult).checked_rem_euclid(stack_size).unwrap()),
+    Ops::CutInto(num) => ((add + *num as i128).checked_rem_euclid(stack_size).unwrap(),mult.checked_rem_euclid(stack_size).unwrap()),
+    Ops::Transpose(num) => ( (add*mod_inv(*num as i128,stack_size)).checked_rem_euclid(stack_size).unwrap(),(mult*mod_inv(*num as i128,stack_size)).checked_rem_euclid(stack_size).unwrap() )
+  };
+  return ret;  
+}
+
 fn inverse_single_transform(op : &Ops, stack_size : i128, output : i128) -> i128 {
   match op {
     Ops::NewStack => stack_size - output - 1,
@@ -79,25 +114,25 @@ fn inverse_single_transform(op : &Ops, stack_size : i128, output : i128) -> i128
   }
 }
 
-fn transform_expr(ops : &Vec<Ops>, stack_size : i128, pos : String) -> String {
-  let mut result = pos;
+// fn transform_expr(ops : &Vec<Ops>, stack_size : i128, pos : String) -> String {
+//   let mut result = pos;
 
-  for op in ops {
-    result = match op {
-      Ops::NewStack => format!("({} - {} - 1)", stack_size, result),
-      Ops::CutInto(num) => format!("({} - {} mod {})", result, num, stack_size),
-      Ops::Transpose(num) => format!("({} * {} mod {})", result, num, stack_size)
-    };
+//   for op in ops {
+//     result = match op {
+//       Ops::NewStack => format!("({} - {} - 1)", stack_size, result),
+//       Ops::CutInto(num) => format!("({} - {} mod {})", result, num, stack_size),
+//       Ops::Transpose(num) => format!("({} * {} mod {})", result, num, stack_size)
+//     };
 
-  }
-  return result;
-}
+//   }
+//   return result;
+// }
 
 pub fn solve_part1(lines : &Vec<String>, stack_size : i128) {
   let mut ops = vec![];
 
   for line in lines {
-    if let Some(args) = NEW_STACK.captures(line) {
+    if let Some(_args) = NEW_STACK.captures(line) {
       // println!("New stack");
       ops.push(Ops::NewStack);
     } else if let Some(args) = CUT_INTO.captures(line) {
@@ -113,19 +148,72 @@ pub fn solve_part1(lines : &Vec<String>, stack_size : i128) {
   }
   
   // println!("{:?}",transform(&ops,stack_size,1));
-  let start = 2019;
-  let mut input = start;
+  let first_number = 2019;
+  let mut input = first_number;
+  let start = Instant::now();
   input = transform(&ops,stack_size,input);
-  println!("result {}", input);
-  println!("reverse part 1 {}", reverse_transform(&ops, stack_size, input));
+  println!("transform in {:?}", start.elapsed());
+  let eq_forward = all_composite(&ops, stack_size);
+  let start = Instant::now();
+  println!("result {} vs {}x+{}={}", input, eq_forward.1, eq_forward.0, (first_number * eq_forward.1 + eq_forward.0).rem_euclid(stack_size));
+  println!("transform in {:?}", start.elapsed());
+  let eq_back = all_inverse_composite(&ops, stack_size);
+  assert!(first_number==(eq_back.1*(eq_forward.1*first_number + eq_forward.0).rem_euclid(stack_size) + eq_back.0).rem_euclid(stack_size));
+  println!("reverse part 1 {} vs {}", reverse_transform(&ops, stack_size, input), (eq_back.1*input + eq_back.0).rem_euclid(stack_size) );
 
+  let mut result = first_number;
+  for _i in 0..10 {
+    result = (( eq_forward.1*result).rem_euclid(stack_size) + eq_forward.0 ).rem_euclid(stack_size);
+  }
+  assert!(first_number==iterate_referse_shuffle(&ops, stack_size, 10, result));  
+}
+fn mod_pow(mut base: i128, mut exp: i128, modulus: i128) -> i128 {
+  if modulus == 1 { return 0 }
+  let mut result = 1;
+  base = base % modulus;
+  while exp > 0 {
+      if exp % 2 == 1 {
+          result = result * base % modulus;
+      }
+      exp = exp >> 1;
+      base = base * base % modulus
+  }
+  result
 }
 
-pub fn solve_part2(lines : &Vec<String>, stack_size : i128) {
+fn solve_the_iteration(add : i128, mult : i128, stack_size : i128, iterations : i128, input : i128) -> i128 {
+
+  let part1 = (mod_pow(mult,iterations,stack_size)*input).rem_euclid(stack_size);
+  let part2 = add.rem_euclid(stack_size);
+  let part3 = ((mod_pow(mult,iterations,stack_size)-1)*mod_inv((mult - 1).rem_euclid(stack_size),stack_size)).rem_euclid(stack_size);
+
+  let test1 = (mult-1).rem_euclid(stack_size);
+  let test2 = mod_inv( (mult - 1).rem_euclid(stack_size),stack_size);
+
+  assert!((test1*test2).rem_euclid(stack_size)==1);
+
+  let mut result = part1 + (part2*part3).rem_euclid(stack_size);
+  result = result.rem_euclid(stack_size);
+  result
+}
+
+fn iterate_referse_shuffle(ops : &Vec<Ops>, stack_size : i128, iterations : i128, output : i128) -> i128 {
+  let (add,mult) = all_inverse_composite(ops, stack_size);
+
+  return solve_the_iteration(add, mult, stack_size, iterations, output);
+}
+
+#[allow(dead_code)]
+fn iterate_shuffle(ops : &Vec<Ops>, stack_size : i128, iterations : i128, input : i128) -> i128 {
+  let (add,mult) = all_composite(ops, stack_size);
+  return solve_the_iteration(add, mult, stack_size, iterations, input);
+}
+
+pub fn solve_part2(lines : &Vec<String>, stack_size : i128, iterations : i128) {
   let mut ops = vec![];
 
   for line in lines {
-    if let Some(args) = NEW_STACK.captures(line) {
+    if let Some(_args) = NEW_STACK.captures(line) {
       // println!("New stack");
       ops.push(Ops::NewStack);
     } else if let Some(args) = CUT_INTO.captures(line) {
@@ -139,20 +227,17 @@ pub fn solve_part2(lines : &Vec<String>, stack_size : i128) {
     }
 
   }
-  
-  let result =  transform(&ops, stack_size,2020);
-  println!("Result = {}",result);
 
-  let calc : i128 = 2020 * 15359977617634; // mod inv of 
-  let mod_inv_iteration = mod_inv(101741582076661, stack_size);
-  println!("{} working with mod_inv compared to {}", mod_inv_iteration, 15359977617634i128);
-  println!("result part 2 {}", reverse_transform(&ops, stack_size, calc));
-  // 92665349929959 is too high
-  // 4978573234469 is too low
 
-  let mut result = 2020;
-  for i in 0..15359977617634i128 {
-    result = reverse_transform(&ops, stack_size, result);
-    if i % 100000 == 0 { println!("{}", i as f64 / 15359977617634i128 as f64);}
+  let mut result_calc;
+
+  let new_eq = all_inverse_composite(&ops, stack_size);
+  result_calc = 2020;
+  for i in 0..10 {  
+    // result = reverse_transform(&ops, stack_size, result);
+    result_calc = ((new_eq.1 * result_calc).rem_euclid(stack_size) + new_eq.0).rem_euclid(stack_size);
+    assert!(result_calc==iterate_referse_shuffle(&ops, stack_size, i+1, 2020));
   }
+  assert!(2020==iterate_shuffle(&ops, stack_size, 10, result_calc));
+  println!("Over {} iterations result is {}", iterations, iterate_referse_shuffle(&ops, stack_size, iterations, 2020).rem_euclid(stack_size));
 }
